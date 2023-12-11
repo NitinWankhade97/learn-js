f1b16a8e4f98b00943eb881a7a0f466d7640b99a
Nitin Wankhade <109677260+NitinWankhade97@users.noreply.github.com>
2023-12-11 17:20:04 +0000

    learn about conversion operations as well as increment and decrement operator

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,68 @@
+let score = "nitin"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+
+let isLoggedIn = "Nitin343"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// true => 1
+// false => 0
+// "" => false
+// "Nitin" => true
+
+let someNumber = " 56"
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************************** Operations *********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "Nitin"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+let x = 3
+let y = x++
+console.log(x);
+console.log(y);
+
+let m = 5
+let n = ++m
+console.log(m);
+console.log(n);
+
+// to learn more..here is the link for increment/decrement operator
+
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
