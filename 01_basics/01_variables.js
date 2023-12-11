f1c0ae034a9c58047000f7b303222e72729d4975
Nitin Wankhade <109677260+NitinWankhade97@users.noreply.github.com>
2023-12-11 13:09:02 +0000

    intro about const,let and var variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,19 @@
-console.log("Nitin");
+const accountId = 133231
+let accountEmail = "nitin@gmail.com"
+var accountPassword = "24214"
+accountCity = "Akola"
+let accountState;
+
+// accountId = 2 // not allowed
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+accountEmail = "abcd@mail.com"
+accountPassword = "331"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword, accountCity, accountState])
