65f72e0240e8ef817ed976f99de3f0a5eba475a4
Nitin Wankhade <109677260+NitinWankhade97@users.noreply.github.com>
2023-12-11 14:06:35 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer versions
+
+// alert(3+3)// we are using node.js, not browser
+
+console.log(3
+    +
+    3); // code readability should be high
+
+console.log("Nitin");
+
+let name = "nitin"
+let age = 26
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigInt
+// string => "" 
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
