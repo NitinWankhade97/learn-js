40e8ca655d27e910e3aaf6f4be75b3f9d7c30aa2
Nitin Wankhade <109677260+NitinWankhade97@users.noreply.github.com>
2024-01-19 10:03:25 +0000

    prototype

---
+++ 10_classes_and_loop/Prototype.js
@@ -0,0 +1,67 @@
+// let myName = "nitin     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.nitin = function(){
+    console.log(`nitin is present in all objects`);
+}
+
+Array.prototype.heyNitin = function(){
+    console.log(`Nitin says hello`);
+}
+
+// heroPower.nitin()
+// myHeros.nitin()
+// myHeros.heyNitin()
+// heroPower.heyNitin()
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"nitin".trueLength()
+"iceTea".trueLength()
